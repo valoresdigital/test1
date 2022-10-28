6ce74dacbe2ebf4d713da1828115ab14e8cd1f96
Nahomi <nahyomi2000@gmail.com>
2022-10-28 18:45:44 -0500

    (feat) Install necesary libraries and font configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { About } from "./components/About/About";
+
 function App() {
   return (
     <div className="App">
       User List 
+      <About/>
     </div>
   );
 }
