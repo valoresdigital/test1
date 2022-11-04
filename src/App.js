abf760f68ee8d6bfd5384200ba5e57995a5adaf9
Mariano <mariano_farace@hotmail.com>
2022-11-04 10:45:35 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const ActiveUsersList = () => {
-  const [users, setUsers] = useState([])
-  
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    const loadUsers = async () => {  
-      const response = await fetch('/some-api')
-      const data = await response.json()
-      setUsers(data)
-    }
-    loadUsers()
-  }, [])
-  
+    const loadUsers = async () => {
+      const response = await fetch("http://localhost:3004/users");
+      const data = await response.json();
+      setUsers(data);
+    };
+    loadUsers();
+  }, []);
+
   const weekAgo = new Date();
   weekAgo.setDate(weekAgo.getDate() - 7);
+
   return (
     <ul>
-      {users.filter(user => !user.isBanned && user.lastActivityAt >= weekAgo).map(user => 
-        <li key={user.id}>
-          <img src={user.avatarUrl} />
-          <p>{user.fullName}</p>
-          <small>{user.role}</small>
-        </li>
-      )}
-    </ul>    
-  )
+      {users
+        .filter(
+          (user) => !user.isBanned && new Date(user.lastActivityAt) >= weekAgo
+        )
+        .map((user) => (
+          <li key={user.id}>
+            <img src={user.avatarUrl} />
+            <p>{user.fullName}</p>
+            <small>{user.role}</small>
+          </li>
+        ))}
+    </ul>
+  );
 }
+
+export default App;
